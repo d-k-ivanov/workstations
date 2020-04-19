b91ac4eb306ec1d8731bab821cd0f3019bcc1892
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-04-19 06:14:52 +0300

    add new Win10-Initial-Setup-Script submodules

---
+++ W10Init.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+PostInstall Configuration.
+
+.DESCRIPTION
+PostInstall Configuration.
+#>
+
+
+# Check invocation
+if ($MyInvocation.InvocationName -ne '.')
+{
+    Write-Host `
+        "Error: Bad invocation. $($MyInvocation.MyCommand) supposed to be sourced. Exiting..." `
+        -ForegroundColor Red
+    Exit
+}
+
+
+$ScriptName         = [io.path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass     `
+  -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"     `
+  -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1" `
+  -preset "${PSScriptRoot}\${ScriptName}.preset"
+
+Set-Service "CDPUserSvc" -StartupType Automatic
