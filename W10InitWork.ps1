4be39009a0a20ff9c2010c265ff1c841e657a328
Dmitry Ivanov <d.k.ivanov@live.com>
2022-08-16 00:52:06 +0300

    Win10-Initial-Setup-Script - refactoring structure

--- W10InitWork.ps1
+++ W10InitWork.ps1
@@ -17,19 +17,24 @@ PostInstall Configuration.
 
 $ScriptName         = [io.path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
 
-if($PSVersionTable.PSEdition -eq "Core")
-{
-    pwsh.exe -NoProfile -ExecutionPolicy Bypass                             `
-        -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"        `
-        -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1"    `
-        -preset "${PSScriptRoot}\${ScriptName}.preset"
-}
-else
-{
-    powershell.exe -NoProfile -ExecutionPolicy Bypass                       `
-        -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"        `
-        -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1"    `
-        -preset "${PSScriptRoot}\${ScriptName}.preset"
-}
+# if($PSVersionTable.PSEdition -eq "Core")
+# {
+#     pwsh.exe -NoProfile -ExecutionPolicy Bypass                             `
+#         -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"        `
+#         -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1"    `
+#         -preset "${PSScriptRoot}\${ScriptName}.preset"
+# }
+# else
+# {
+#     powershell.exe -NoProfile -ExecutionPolicy Bypass                       `
+#         -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"        `
+#         -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1"    `
+#         -preset "${PSScriptRoot}\${ScriptName}.preset"
+# }
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass                       `
+    -File "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.ps1"        `
+    -include "${PSScriptRoot}\Win10-Initial-Setup-Script\Win10.psm1"    `
+    -preset "${PSScriptRoot}\${ScriptName}.preset"
 
 Set-Service "CDPUserSvc" -StartupType Automatic
