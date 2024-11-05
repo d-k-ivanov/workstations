f0c3e64be5f2f653ff6a28f767e4edb1d7569bd0
Dmitry Ivanov <d.k.ivanov@live.com>
2024-11-05 19:06:15 +0100

    workstations: 2024-11-05-16-22

--- ubuntu/bin/gpg.sh
+++ ubuntu/bin/gpg.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 f_usage() {
-    local Y="\033[0;33m"    # Yellow
-    local ZZ="\033[0m"      # Reset
+    local Y="\033[0;33m" # Yellow
+    local ZZ="\033[0m"   # Reset
     printf "${Y}Usage: $(basename $0) <option>${ZZ}\n\n"
     printf "${Y}  Options:
     [e]ncrypt
     [d]ecrypt${ZZ}\n"
 }
 
-ANSIBLE_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+ANSIBLE_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
 declare -a secured_files=(
     "${ANSIBLE_HOME}/secrets/ansible_password"
 )
 
 declare -a recipients=()
-for key in `ls ${ANSIBLE_HOME}/gpg`; do
+for key in $(ls ${ANSIBLE_HOME}/gpg); do
+    # gpg --import "gpg/${key}"
     recipients+="-r $(basename -- ${key%.*}) "
 done
 
@@ -26,21 +27,22 @@ echo "Trusted persons: ${recipients}"
 [[ -z ${gpg_exe}    ]] && gpg_exe='gpg'
 
 case $1 in
-    e|encrypt )
+    e | encrypt)
         shift
         for file in "${secured_files[@]}"; do
             printf "\033[0;32mEncrypting $file\033[0m\n"
+            echo ${gpg_exe} -e --yes --trust-model always "${recipients}" "${file}"
             ${gpg_exe} -e --yes --trust-model always "${recipients}" "${file}"
         done
         ;;
-    d|decrypt )
+    d | decrypt)
         shift
         for file in "${secured_files[@]}"; do
             printf "\033[0;32mDecrypting $file\033[0m\n"
             ${gpg_exe} --output ${file} --decrypt ${file}.gpg
         done
         ;;
-    * )
+    *)
         f_usage
         ;;
 esac
