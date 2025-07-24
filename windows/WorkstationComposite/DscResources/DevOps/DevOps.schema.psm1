f70034874ccc405da1ea1387885e903389c9b3d7
Dmitry Ivanov <d.k.ivanov@live.com>
2025-07-24 12:04:36 +0200

    windows: 2025-24-07-12-04

--- windows/WorkstationComposite/DscResources/DevOps/DevOps.schema.psm1
+++ windows/WorkstationComposite/DscResources/DevOps/DevOps.schema.psm1
@@ -98,8 +98,8 @@ Configuration DevOps
     Script DownloadBindToolsConfig
     {
         SetScript  = {
-            Write-Output 'nameserver 8.8.8.8' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
-            Write-Output 'nameserver 77.88.8.8' >> $env:SystemRoot\System32\Drivers\etc\resolv.conf
+            Write-Output 'nameserver 1.1.1.1' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
+            Write-Output 'nameserver 8.8.8.8' >> $env:SystemRoot\System32\Drivers\etc\resolv.conf
         }
         GetScript  = { @{} }
         TestScript = {
