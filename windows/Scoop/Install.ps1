2a4df681bf85c3e4c5a26f07be5773ec0898735f
Dmitry Ivanov <d.k.ivanov@live.com>
2025-08-13 00:45:37 +0200

    windows: 2025-13-08-00-45

--- windows/Scoop/Install.ps1
+++ windows/Scoop/Install.ps1
@@ -1,4 +1,21 @@
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-scoop install main/nmap
-scoop install main/msys2
+if (! (Get-Command scoop -ErrorAction SilentlyContinue))
+{
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+else
+{
+    Write-Host "Installing buckets..."
+    scoop bucket add extras
+    scoop bucket add games
+    scoop bucket add nerd-fonts
+
+    Write-Host "Installing Software with Scoop..."`
+    scoop install main/nmap
+    # scoop install main/msys2
+
+    # Fonts
+    scoop install nerd-fonts/Hack-NF
+    scoop install nerd-fonts/Hack-NF-Propo
+    scoop install nerd-fonts/Hack-NF-Mono
+}
